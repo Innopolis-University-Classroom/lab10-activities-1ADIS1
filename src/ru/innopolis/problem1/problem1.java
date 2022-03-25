b72cca75b43612b6009698c0e048f1db38c2a80a
1ADIS1 <60191045+1ADIS1@users.noreply.github.com>
2022-03-25 17:54:13 +0300

    Draft of shape factory

--- src/ru/innopolis/problem1/problem1.java
+++ src/ru/innopolis/problem1/problem1.java
@@ -1,2 +1,55 @@
-// your solution here
-//fix all the errors including package errors
+interface ShapeFactory {
+    Shape createShape();
+}
+
+class CircleFactory implements ShapeFactory {
+    @Override
+    public Circle createShape() {
+        return new Circle();
+    }
+}
+
+abstract class Shape {
+    public abstract double getArea();
+}
+
+class Circle extends Shape {
+    public double radius;
+
+    public Circle() {
+        this.radius = 0;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Square extends Shape {
+    public double length;
+
+    public Square() {
+        this.length = 0;
+    }
+
+    @Override
+    public double getArea() {
+        return length * length;
+    }
+}
+
+class Rectangle extends Shape {
+    public double length;
+    public double width;
+
+    public Rectangle() {
+        this.length = 0;
+        this.width = 0;
+    }
+
+    @Override
+    public double getArea() {
+        return length * width;
+    }
+}
